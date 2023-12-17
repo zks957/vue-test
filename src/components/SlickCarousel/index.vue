f27fdd8f587fae3fa8306a0e55b41c3928c3cd5d
zzks1980 <2794477950@qq.com>
2023-12-17 15:46:37 +0800

    first commit

---
+++ src/components/SlickCarousel/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Slick from 'vue-slick';
+import { ref } from 'vue';
+// Events listeners
+const handleAfterChange = (event:any, slick:any, currentSlide:any) => {
+    console.log('handleAfterChange', event, slick, currentSlide);
+};
+
+const slickOptions = ref({
+                            slidesToShow: 3,
+                            // Any other options that can be got from plugin documentation
+                        }) 
+</script>
+
+<template>
+    <slick
+      ref="slick"
+      :options="slickOptions"
+      @afterChange="handleAfterChange">
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+      <div class="item"></div>
+    </slick>
+</template>
+
+<style scoped>
+.item {
+  height: 100px;
+  width: 500px;
+  background-color: antiquewhite;
+  border: 1px solid #fff;
+}
+</style>
---
+++ src/components/VModelTest/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+import { computed } from 'vue'
+
+const emits = defineEmits(['update:inputValue'])
+const props = defineProps({
+    inputValue:String
+}) 
+
+const handerChange = (e:Event)=>{
+    const value = (e.target as HTMLInputElement).value;
+    emits('update:inputValue',value);
+    console.log(value);
+}
+
+let value = computed(()=>{
+    return props.inputValue;
+})
+
+</script>
+
+<template>
+    <input :value="value" @input="handerChange" />
+</template>
+
+<style scoped>
+
+</style>
